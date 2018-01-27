ad47016f507c871bce5af71caed050e7339df80b
parvir <parvirchomber@gmail.com>
2018-01-27 13:32:55 +0000

    update pop script

--- populate_rango.py
+++ populate_rango.py
@@ -10,6 +10,7 @@ def populate(): #Populate method I am assuming
 
 
     python_pages = [
+
         {"title": "Official Python Tutorial",
          "url": "http://docs.python.org/2/tutorial/"},
         {"title": "How to Think like a Computer Scientist",
@@ -33,18 +34,16 @@ def populate(): #Populate method I am assuming
         {"title": "Flask",
          "url": "http://flask.pocoo.org"}
     ]
-
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages},
-            #"Views": {"Views"}, #Correct?
-            #"Likes": {"Likes"}
+                            
+    cats = {"Python": {"pages" : python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
             }
 
-    for cat, cat_data in cats.items():
-        c = add_cat(cat,views,likes)
+    for cat, cat_data in cats.items():  #for loop
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -58,7 +57,7 @@ def add_page(cat, title, url, views=0):
     return p
 #Get or create checks that there is no duplicates
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=0, likes=0)[0]
+    c = Category.objects.get_or_create(name=name)[0]
     c.views=views
     c.likes=likes
     c.save()
