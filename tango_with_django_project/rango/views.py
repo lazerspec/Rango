e0b6d94d97f95451994eeae08aa0238be6863871
parvir <parvirchomber@gmail.com>
2018-01-31 16:01:09 +0000

    Working through ex 3

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from rango.models import Category
+
+def index (request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    #  Retrieve the top 5 only - or all if less than 5.
+    #  Place the list in our context_dict dictionary
+    #  that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+
+    return render(request, 'rango/index.html', context = context_dict)
+   #return HttpResponse("Rango says hey there partner! ")
+
+def about(request):
+
+    #context_dict = { 'Rango says here is the about page.'}
+
+    return HttpResponse("Rango  says here is the about page. <a href='/rango/'>View index page</a>")  #The HTML link is a reference back to the starter page
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,6 +14,18 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static') #Telling django where to find these directories
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +67,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR, ],             #Empty initially. Changing
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'  #added
             ],
         },
     },
@@ -113,6 +127,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
