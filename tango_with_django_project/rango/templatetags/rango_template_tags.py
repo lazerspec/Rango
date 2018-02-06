d5add64bd1021c7d78c07a8ffd296dd8cfdfc316
parvir <parvirchomber@gmail.com>
2018-02-06 20:04:33 +0000

    end of chap8

--- tango_with_django_project/rango/templatetags/rango_template_tags.py
+++ tango_with_django_project/rango/templatetags/rango_template_tags.py
@@ -4,6 +4,6 @@ from rango.models import Category
 register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
-def get_category_list():    #returns list of categories
-    return {'cats': Category.objects.all()}
+def get_category_list(cat=None):    #returns list of categories
+    return {'cats': Category.objects.all(), 'act_cat': cat}
 
