efdf34fabbc6c5f453475d4a992b32e0d0fe90b9
parvir <parvirchomber@gmail.com>
2018-02-13 15:37:51 +0000

    fixing bugs

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static') #Telling django where to find these directories
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
